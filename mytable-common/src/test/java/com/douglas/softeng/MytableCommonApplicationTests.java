244d9bbf25ccda712da81de825f6c4023ede237b
petruki <31597636+petruki@users.noreply.github.com>
2021-02-28 13:01:04 -0800

    Fixed common dependency

--- mytable-common/src/test/java/com/douglas/softeng/MytableCommonApplicationTests.java
+++ mytable-common/src/test/java/com/douglas/softeng/MytableCommonApplicationTests.java
@@ -1,9 +1,7 @@
 package com.douglas.softeng;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class MytableCommonApplicationTests {
 
 	@Test
